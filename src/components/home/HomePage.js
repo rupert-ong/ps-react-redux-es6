c8938a8512515eb507f3fb2dddd25eaf1791e36d
rupert-ong <rupert.ong@gmail.com>
2019-03-20 08:50:08 -0400

    Add Header with NavLinks

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => (
   <div className="jumbotron">
     <h1>Pluralsight Administration</h1>
     <p>React, Redux and React Router for ultra-responsive web apps</p>
-    <Link to="about" className="btn btn-primary btn-lg">
+    <Link to="/about" className="btn btn-primary btn-lg">
       Learn More
     </Link>
   </div>
