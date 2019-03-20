1867a3d46eff996a2e3e05cd9b0babb1354b5bba
rupert-ong <rupert.ong@gmail.com>
2019-03-20 08:57:05 -0400

    Add Courses Page component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './common/Header';
 import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
+import CoursesPage from './courses/CoursesPage';
 import PageNotFound from './PageNotFound';
 
 const App = () => (
@@ -12,6 +13,7 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
+      <Route path="/courses" component={CoursesPage} />
       <Route component={PageNotFound} />
     </Switch>
   </div>
---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class CoursesPage extends Component {
+  render() {
+    return <h2>Courses Page</h2>;
+  }
+}
+
+export default CoursesPage;
