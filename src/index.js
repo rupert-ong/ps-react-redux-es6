55a10fb798d09510c111da2bcc64fedbad68794f
rupert-ong <rupert.ong@gmail.com>
2019-03-19 22:01:00 -0400

    Add BrowserRouter and styles to entry point JS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 function Hi() {
   return <p>Hi</p>;
 }
 
-render(<Hi />, document.getElementById('app'));
+render(
+  <Router>
+    <Hi />
+  </Router>,
+  document.getElementById('app')
+);
