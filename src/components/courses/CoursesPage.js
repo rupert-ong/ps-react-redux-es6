477cf9175c20b4034e890f59dcb9cb7b3ba51a84
rupert-ong <rupert.ong@gmail.com>
2019-03-20 16:04:19 -0400

    Setup CoursesPage form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 
 class CoursesPage extends Component {
+  state = {
+    course: {
+      title: ''
+    }
+  };
+
+  handleChange = e => {
+    const course = {
+      ...this.state.course,
+      title: e.target.value
+    };
+    this.setState({ course });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses Page</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
