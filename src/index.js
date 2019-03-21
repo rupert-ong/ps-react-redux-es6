b933dac544e749cf01681bc14195f8058d6602a1
rupert-ong <rupert.ong@gmail.com>
2019-03-20 21:34:53 -0400

    Apply Redux provider to App

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+
 import App from './components/App';
+
+import configureStore from './redux/configureStore';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+// Can pass in initial state here (server, server-side rendering, localStorage, etc.)
+const store = configureStore();
+
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById('app')
 );
