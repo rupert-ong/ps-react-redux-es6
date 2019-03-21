a633cfd3061731500a389e39ff8aba51d0aabb13
rupert-ong <rupert.ong@gmail.com>
2019-03-21 15:09:18 -0400

    Remove add from CoursesPage container

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -6,40 +6,14 @@ import { bindActionCreators } from 'redux';
 import * as courseActions from '../../redux/actions/courseActions';
 
 class CoursesPage extends Component {
-  state = {
-    course: {
-      title: ''
-    }
-  };
-
-  handleChange = e => {
-    const course = {
-      ...this.state.course,
-      title: e.target.value
-    };
-    this.setState({ course });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
         {this.props.courses.map(course => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
