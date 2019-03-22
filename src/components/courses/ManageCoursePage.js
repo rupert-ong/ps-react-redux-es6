56a8083f817255b8104a9b81cb6bb6baeed43237
rupert-ong <rupert.ong@gmail.com>
2019-03-22 11:07:23 -0400

    Add ManageCoursePage component

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loadCourses } from '../../redux/actions/courseActions';
+import { loadAuthors } from '../../redux/actions/authorActions';
+import PropTypes from 'prop-types';
+
+class ManageCoursePage extends Component {
+  componentDidMount() {
+    const { courses, authors, loadCourses, loadAuthors } = this.props;
+
+    if (courses.length === 0) {
+      loadCourses().catch(error => alert(`Loading courses failed: ${error}`));
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch(error => alert(`Loading authors failed: ${error}`));
+    }
+  }
+  render() {
+    return (
+      <>
+        <h2>Manage Course</h2>
+      </>
+    );
+  }
+}
+
+ManageCoursePage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors
+  };
+}
+
+// Object mapping of mapDispatchToProps automatically wraps functions in Redux's dispatch()
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ManageCoursePage);
