686ce0de8ec042ebc051adb7b4362d90229c5110
rupert-ong <rupert.ong@gmail.com>
2019-03-22 11:09:56 -0400

    Add ManageCourse page route

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Header from './common/Header';
 import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
 import CoursesPage from './courses/CoursesPage';
+import ManageCoursePage from './courses/ManageCoursePage';
 import PageNotFound from './PageNotFound';
 
 const App = () => (
@@ -14,6 +15,8 @@ const App = () => (
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
       <Route path="/courses" component={CoursesPage} />
+      <Route path="/course/:slug" component={ManageCoursePage} />
+      <Route path="/course/" component={ManageCoursePage} />
       <Route component={PageNotFound} />
     </Switch>
   </div>
