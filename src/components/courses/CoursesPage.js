c9600fefdaa84dce3f64bd027da6b1071b995788
rupert-ong <rupert.ong@gmail.com>
2019-03-21 21:34:58 -0400

    Set up author redux. Display course & author data.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -4,21 +4,32 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 
 import * as courseActions from '../../redux/actions/courseActions';
+import * as authorActions from '../../redux/actions/authorActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends Component {
   componentDidMount() {
-    this.props.actions
-      .loadCourses()
-      .catch(error => alert('Loading courses failed ' + error));
+    const { courses, authors, actions } = this.props;
+
+    // Ensure to only request async data if it doesn't exist in the store
+    if (courses.length === 0) {
+      actions
+        .loadCourses()
+        .catch(error => alert('Loading courses failed ' + error));
+    }
+
+    if (authors.length === 0) {
+      actions
+        .loadAuthors()
+        .catch(error => alert('Loading authors failed ' + error));
+    }
   }
 
   render() {
     return (
       <>
         <h2>Courses</h2>
-        {this.props.courses.map(course => (
-          <div key={course.title}>{course.title}</div>
-        ))}
+        <CourseList courses={this.props.courses} />
       </>
     );
   }
@@ -26,22 +37,36 @@ class CoursesPage extends Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  dispatch: PropTypes.func.isRequired,
+  authors: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
   return {
     // Request only the store data you need to avoid needless re-renders
-    courses: state.courses
+    // Enhance courses to add author name (courses contains normalized id info on author, not author name)
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map(course => {
+            return {
+              ...course,
+              authorName: state.authors.find(
+                author => author.id === course.authorId
+              ).name
+            };
+          }),
+    authors: state.authors
   };
 }
 
 // bindActionCreators mapping of dispatch actions to mapDispatchToProps
 function mapDispatchToProps(dispatch) {
   return {
-    // Automatically wrap all action creators in a dispatch call
-    actions: bindActionCreators(courseActions, dispatch)
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    }
   };
 }
 
