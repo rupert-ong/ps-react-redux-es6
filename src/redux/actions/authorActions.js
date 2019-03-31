8308ba574b18012a1c13a1ed4de1a167cef2fec4
rupert-ong <rupert.ong@gmail.com>
2019-03-30 22:10:07 -0400

    Add save author action type, creator and reducer

--- src/redux/actions/authorActions.js
+++ src/redux/actions/authorActions.js
@@ -6,6 +6,10 @@ export function loadAuthorsSuccess(authors) {
   return { type: types.LOAD_AUTHORS_SUCCESS, authors };
 }
 
+export function createAuthorSuccess(author) {
+  return { type: types.CREATE_AUTHOR_SUCCESS, author };
+}
+
 export function deleteAuthorOptimistic(author) {
   return { type: types.DELETE_AUTHOR_OPTIMISTIC, author };
 }
@@ -18,6 +22,21 @@ export function loadAuthors() {
       .getAuthors()
       .then(authors => {
         dispatch(loadAuthorsSuccess(authors));
+      })
+      .catch(error => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
+
+export function saveAuthor(author) {
+  return function(dispatch) {
+    dispatch(beginApiCall());
+    return authorApi
+      .saveAuthor(author)
+      .then(savedAuthor => {
+        dispatch(createAuthorSuccess(savedAuthor));
       })
       .catch(error => {
         dispatch(apiCallError(error));
