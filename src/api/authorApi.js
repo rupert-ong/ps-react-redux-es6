04a670ca92580a52ef48f56cebcce8939358b6dd
rupert-ong <rupert.ong@gmail.com>
2019-03-30 22:08:42 -0400

    Add saveAuthor method to authorApi.js

--- src/api/authorApi.js
+++ src/api/authorApi.js
@@ -7,6 +7,16 @@ export function getAuthors() {
     .catch(handleError);
 }
 
+export function saveAuthor(author) {
+  return fetch(baseUrl + (author.id || ''), {
+    method: author.id ? 'PUT' : 'POST', // POST for create, PUT to update when id already exists.
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(author)
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
 export function deleteAuthor(authorId) {
   return fetch(baseUrl + authorId, { method: 'DELETE' })
     .then(handleResponse)
