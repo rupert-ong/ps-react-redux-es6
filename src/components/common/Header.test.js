92ec007904a439f3dfdf3f9988e4c50fbf6df220
rupert-ong <rupert.ong@gmail.com>
2019-03-31 12:59:24 -0400

    Update Header test case descriptions

--- src/components/common/Header.test.js
+++ src/components/common/Header.test.js
@@ -4,14 +4,14 @@ import { shallow, mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 
 // Can search for React Component using Shallow render
-it('contains 3 NavLinks via shallow', () => {
+it('contains 4 NavLinks via shallow', () => {
   const numLinks = shallow(<Header />).find('NavLink').length;
   expect(numLinks).toEqual(4);
 });
 
 // Must search the final rendered HTML in Mounted render as DOM is generated
 // Need React Routers' memoryRouter to pull in props passed into Header
-it('contains 3 anchors via mount', () => {
+it('contains 4 anchors via mount', () => {
   const numAnchors = mount(
     <MemoryRouter>
       <Header />
