8afc501636b1d52441f119f9378906a33660126e
rupert-ong <rupert.ong@gmail.com>
2019-03-30 22:08:05 -0400

    Add POST authors endpoint

--- tools/apiServer.js
+++ tools/apiServer.js
@@ -51,6 +51,16 @@ server.post('/courses/', function(req, res, next) {
   }
 });
 
+server.post('/authors/', function(req, res, next) {
+  const error = validateAuthor(req.body);
+  if (error) {
+    res.status(400).send(error);
+  } else {
+    req.body.slug = createSlug(req.body.name);
+    next();
+  }
+});
+
 // Use default router
 server.use(router);
 
@@ -74,5 +84,10 @@ function validateCourse(course) {
   if (!course.title) return 'Title is required.';
   if (!course.authorId) return 'Author is required.';
   if (!course.category) return 'Category is required.';
+  return '';
+}
+
+function validateAuthor(author) {
+  if (!author.name) return 'Author is required';
   return '';
 }
