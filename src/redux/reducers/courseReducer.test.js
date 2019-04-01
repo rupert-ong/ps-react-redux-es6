f336a346f07e6f434ea11efea70b766b23bd3b78
rupert-ong <rupert.ong@gmail.com>
2019-03-31 21:04:21 -0400

    Fix courseReducer tests

--- src/redux/reducers/courseReducer.test.js
+++ src/redux/reducers/courseReducer.test.js
@@ -1,37 +1,55 @@
 import courseReducer from './courseReducer';
 import * as actions from '../actions/courseActions';
 
-it('should add course when passed CREATE_COURSE_SUCCESS', () => {
-  const initialState = [{ title: 'A' }, { title: 'B' }];
-  const newCourse = { title: 'C' };
+describe('courseReducer Tests', () => {
+  let initialState;
+  let newCourse;
 
-  const action = actions.createCourseSuccess(newCourse);
+  beforeAll(() => {
+    initialState = [
+      { id: 1, title: 'A' },
+      { id: 2, title: 'B' },
+      { id: 3, title: 'C' }
+    ];
+    newCourse = {
+      id: 4,
+      title: 'D'
+    };
+  });
 
-  const newState = courseReducer(initialState, action);
+  it('should add course when passed CREATE_COURSE_SUCCESS', () => {
+    const action = actions.createCourseSuccess(newCourse);
+    const newState = courseReducer(initialState, action);
 
-  expect(newState.length).toEqual(3);
-  expect(newState[0].title).toEqual('A');
-  expect(newState[1].title).toEqual('B');
-  expect(newState[2].title).toEqual('C');
-});
+    expect(newState.length).toEqual(4);
+    expect(newState[0].title).toEqual('A');
+    expect(newState[1].title).toEqual('B');
+    expect(newState[2].title).toEqual('C');
+    expect(newState[3].title).toEqual('D');
+  });
 
-it('should update course when passed UPDATE_COURSE_SUCCESS', () => {
-  // setup
-  const initialState = [
-    { id: 1, title: 'A' },
-    { id: 2, title: 'B' },
-    { id: 3, title: 'B' }
-  ];
-  const course = { id: 2, title: 'New Title' };
+  it('should update course when passed UPDATE_COURSE_SUCCESS', () => {
+    const course = { id: 2, title: 'New Title' };
 
-  // act
-  const action = actions.updateCourseSuccess(course);
-  const newState = courseReducer(initialState, action);
-  const updatedCourse = newState.find(a => a.id === course.id);
-  const untouchedCourse = newState.find(a => a.id === 1);
+    const action = actions.updateCourseSuccess(course);
+    const newState = courseReducer(initialState, action);
 
-  // assert
-  expect(updatedCourse.title).toEqual('New Title');
-  expect(untouchedCourse.title).toEqual('A');
-  expect(newState.length).toEqual(3);
+    const updatedCourse = newState.find(a => a.id === course.id);
+    const untouchedCourse = newState.find(a => a.id === 1);
+
+    expect(updatedCourse.title).toEqual('New Title');
+    expect(untouchedCourse.title).toEqual('A');
+    expect(newState.length).toEqual(3);
+  });
+
+  it('should delete a course when passed DELETE_COURSE_OPTIMISITC', () => {
+    const course = { id: 2, name: 'B' };
+
+    const action = actions.deleteCourseOptimistic(course);
+    const newState = courseReducer(initialState, action);
+
+    expect(newState[0].title).toEqual('A');
+    expect(newState[1].title).toEqual('C');
+    expect(newState.length).toEqual(2);
+  });
 });
